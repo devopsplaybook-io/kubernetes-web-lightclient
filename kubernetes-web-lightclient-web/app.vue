1efd417041f71253c51b7dd6f6f6fc00d3c92de7
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2026-05-09 20:56:47 +0800

    wip

--- kubernetes-web-lightclient-web/app.vue
+++ kubernetes-web-lightclient-web/app.vue
@@ -95,6 +95,65 @@ main {
   }
 }
 
+/* Loading */
+
+@media (prefers-color-scheme: dark) {
+  .loading-indicator {
+    --c: no-repeat linear-gradient(#bcc6ce 0 0);
+  }
+}
+@media (prefers-color-scheme: light) {
+  .loading-indicator {
+    --c: no-repeat linear-gradient(#1d2832 0 0);
+  }
+}
+.loading-indicator {
+  width: 15%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20%;
+  margin-bottom: 20%;
+  aspect-ratio: 1;
+  background:
+    var(--c) 0% 50%,
+    var(--c) 50% 50%,
+    var(--c) 100% 50%;
+  background-size: 20% 100%;
+  animation: l1 2s infinite linear;
+}
+@keyframes l1 {
+  0% {
+    background-size:
+      20% 100%,
+      20% 100%,
+      20% 100%;
+  }
+  33% {
+    background-size:
+      20% 10%,
+      20% 100%,
+      20% 100%;
+  }
+  50% {
+    background-size:
+      20% 100%,
+      20% 10%,
+      20% 100%;
+  }
+  66% {
+    background-size:
+      20% 100%,
+      20% 100%,
+      20% 10%;
+  }
+  100% {
+    background-size:
+      20% 100%,
+      20% 100%,
+      20% 100%;
+  }
+}
+
 .vue-apexcharts,
 [role="button"] {
   padding: 0;
